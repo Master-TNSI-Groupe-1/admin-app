377ba99104878601145812435aa17f6faf3ad180
Rémy Halipré <v.remy.halipre@gmail.com>
2019-12-12 13:43:44 +0100

    init

--- src/main/java/com/application/controller/MainController.java
+++ src/main/java/com/application/controller/MainController.java
@@ -3,11 +3,10 @@ package com.application.controller;
 import com.application.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class MainController {
 
 	@Autowired
 	TestService testService;
@@ -16,12 +15,12 @@ public class HomeController {
 	public String test() {
 		System.out.println("On est ici");
 		System.out.println(testService.getAllTest());
-		return "index";
+		return "main";
 	}
 	
 	@RequestMapping("/")
 	public String getHome() {
-		return "index";
+		return "main";
 	}
 	
 	@RequestMapping("/authenticated")
